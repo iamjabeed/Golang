adde894f819016b53f8a4957144c69a18c95ad9e
Jabeed Syed <jabeed.syed@hexanovamedia.tech>
2024-05-27 20:59:11 +0530

    first program and variables

---
+++ index.go
@@ -0,0 +1,37 @@
+package main
+
+// HelloWorld
+// func main() {
+// 	fmt.Println("Hello, world!")
+// }
+
+// 1.Variables
+// Go Variable Naming Rules
+// - A variable name must start with a letter or an underscore character (_)
+// - A variable name cannot start with a digit
+// - A variable name can only contain alpha-numeric characters and underscores (a-z, A-Z, 0-9, and _ )
+// - Variable names are case-sensitive (age, Age and AGE are three different variables)
+// - There is no limit on the length of the variable name
+// - A variable name cannot contain spaces
+// - The variable name cannot be any Go keywords
+
+var name ="Jabeed"
+
+// func main() {
+// 	var movie ="Batman"
+// 	var country string ="INDIA"
+// 	var age int =24
+// 	var height float64= 5.10
+// 	var isDeveloper = true
+// 	fmt.Println(movie,country,age,height,isDeveloper,"name:", name )
+// }
+
+// shorthand declaration for varables
+// func main() {
+//     name := "Jabeed"
+//     age := 25
+//     height := 5.9
+//     isDeveloper := true
+
+//     fmt.Println(name, age, height, isDeveloper)
+// }
